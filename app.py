76a759b856fdc7ea477eadbe3ba5f900c2988d02
Siddhant Sharma <135413969+CODE-HUB07@users.noreply.github.com>
2025-09-25 21:29:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import random  # for simulating healthy/diseased leaf
 app = Flask(__name__)
 
 # Explicitly allow CORS for your frontend
-CORS(app, resources={r"/predict": {"origins": "https://courageous-kulfi-7f2081.netlify.app/"}})
+CORS(app, resources={r"/predict": {"origins": "https://rnd-school.netlify.app/"}})
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -39,5 +39,6 @@ def predict():
 if __name__ == '__main__':
     # Listen on all interfaces for Render deployment
     app.run(host='0.0.0.0', port=5000)
+
 
 
